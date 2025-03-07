5e390429f57fd9effc63effbe4a5ae008df19b20
Zeynel <zeynel.koca@outlook.com>
2025-03-07 23:00:13 +0100

    videomae train and inference logic

--- ml/game_state/videomae.py
+++ ml/game_state/videomae.py
@@ -1,15 +1,24 @@
+from argparse import ArgumentParser, BooleanOptionalAction
 from enum import Enum
 import imageio
+import evaluate
 import os
 import pathlib
+import numpy as np
 import tarfile
 from matplotlib import animation
 import matplotlib.pyplot as plt
-from PIL import Image, ImageFile
 from huggingface_hub import hf_hub_download
 from numpy import ndarray
 from torch import Tensor
-from transformers import VideoMAEImageProcessor, VideoMAEForVideoClassification
+import torch
+from transformers import (
+    VideoMAEImageProcessor,
+    VideoMAEForVideoClassification,
+    TrainingArguments,
+    Trainer,
+    pipeline,
+)
 from definitions import ROOT_DIR
 import pytorchvideo.data
 from pytorchvideo.data.labeled_video_dataset import LabeledVideoDataset
@@ -35,7 +44,10 @@ class Dataset(Enum):
     EVALUATE = "test"
 
 
-def use_pretrained_hf_model():
+metric = evaluate.load("accuracy")
+
+
+def main(train_new_model: bool, model_name: str, inference: bool, show_gif: bool):
     dataset_path = download_hf_dataset()
 
     with tarfile.open(dataset_path) as t:
@@ -43,8 +55,7 @@ def use_pretrained_hf_model():
         print(f"Extracting hugging face dataset into {extraction_path}")
         t.extractall(extraction_path)
 
-        dataset_root_path = pathlib.Path(extraction_path, "UCF101_subset")
-
+    dataset_root_path = pathlib.Path(extraction_path, "UCF101_subset")
     all_video_file_paths = (
         list(dataset_root_path.glob("train/*/*.avi"))
         + list(dataset_root_path.glob("val/*/*.avi"))
@@ -58,7 +69,27 @@ def use_pretrained_hf_model():
 
     print(f"Unique class labels: {list(label2id.keys())}.")
 
-    train_model(label2id, id2label, dataset_root_path)
+    model_checkpoint = "MCG-NJU/videomae-base"  # Huggingface videomae as base model
+    image_processor, model = load_model(model_checkpoint, label2id, id2label)
+    train_ds, val_ds, test_ds = get_datasets(image_processor, model, dataset_root_path)
+
+    if train_new_model:
+        print("Start training new model...")
+        train_results = train_model(
+            model, image_processor, model_name, train_ds, val_ds
+        )
+
+    if inference:
+        print("Start running inference...")
+        run_inference(
+            model_name=model_name,
+            test_dataset=test_ds,
+            show_gif=show_gif,
+            gif_mean=image_processor.image_mean,
+            gif_std=image_processor.image_std,
+            dataset_root_path=dataset_root_path,
+            id2label=id2label,
+        )
 
 
 def download_hf_dataset() -> str:
@@ -72,10 +103,96 @@ def download_hf_dataset() -> str:
 
 
 def train_model(
-    label2id: dict[str, int], id2label: dict[int, str], dataset_root_path: str
+    model: VideoMAEForVideoClassification,
+    image_processor: VideoMAEImageProcessor,
+    new_model_name: str,
+    training_dataset: LabeledVideoDataset,
+    validation_dataset: LabeledVideoDataset,
+    num_epochs: int = 20,
+    batch_size: int = 8,
 ):
-    image_processor, model = load_model(label2id, id2label)
+    args = TrainingArguments(
+        output_dir=new_model_name,
+        remove_unused_columns=False,
+        eval_strategy="epoch",
+        save_strategy="epoch",
+        learning_rate=5e-5,
+        per_device_train_batch_size=batch_size,
+        per_device_eval_batch_size=batch_size,
+        warmup_ratio=0.1,
+        lr_scheduler_type="cosine",  # Often works better than linear
+        logging_steps=10,
+        load_best_model_at_end=True,
+        use_cpu=False,
+        fp16=True,  # Exponentially increases training speed on cuda gpu
+        dataloader_num_workers=8,  # uses more cpu cores for data loading
+        metric_for_best_model="accuracy",
+        max_steps=(training_dataset.num_videos // batch_size) * num_epochs,
+    )
 
+    trainer = Trainer(
+        model=model,
+        args=args,
+        train_dataset=training_dataset,
+        eval_dataset=validation_dataset,
+        processing_class=image_processor,
+        compute_metrics=compute_metrics,
+        data_collator=collate_fn,
+    )
+
+    return trainer.train()
+
+
+def run_inference(
+    model_name: str,
+    test_dataset: LabeledVideoDataset,
+    show_gif: bool,
+    gif_mean: float,
+    gif_std: float,
+    dataset_root_path: str,
+    id2label: dict[int, str],
+):
+    video_classifier = pipeline(model=model_name, task="video-classification", device=0)
+
+    for idx, sample_video in enumerate(test_dataset):
+        print(f"Processing video {idx + 1} / {test_dataset.num_videos}")
+
+        if show_gif:
+            video_tensor = sample_video["video"]
+            display_gif(video_tensor, gif_mean, gif_std)
+
+        video_name = sample_video["video_name"]
+        video_label = id2label[sample_video["label"]]
+        video_path = os.path.join(dataset_root_path, "test", video_label, video_name)
+        inference_result = video_classifier(video_path)
+
+        success = inference_result[0]["label"] == video_label
+        printColor = "\033[94m" if success else "\033[91m"
+        print(
+            f"{printColor}Inference result: {inference_result} on video {video_path} with actual label {video_label}"
+        )
+
+
+def compute_metrics(eval_pred):
+    predictions = np.argmax(eval_pred.predictions, axis=1)
+    return metric.compute(predictions=predictions, references=eval_pred.label_ids)
+
+
+def collate_fn(examples):
+    # permute to (num_frames, num_channels, height, width)
+    pixel_values = torch.stack(
+        [example["video"].permute(1, 0, 2, 3) for example in examples]
+    )
+
+    labels = torch.tensor([example["label"] for example in examples])
+    return {"pixel_values": pixel_values, "labels": labels}
+
+
+def get_datasets(
+    image_processor: VideoMAEImageProcessor,
+    model: VideoMAEForVideoClassification,
+    dataset_root_path: str,
+):
     mean = image_processor.image_mean
     std = image_processor.image_std
     if "shortest_edge" in image_processor.size:
@@ -122,9 +239,7 @@ def train_model(
     )
     print(f"Created evaluation dataset with {test_dataset.num_videos} videos")
 
-    sample_video = next(iter(train_dataset))
-    video_tensor = sample_video["video"]
-    display_gif(video_tensor, mean, std)
+    return train_dataset, val_dataset, test_dataset
 
 
 def create_dataset(
@@ -150,6 +265,10 @@ def create_dataset(
                 RandomHorizontalFlip(p=0.5),
             ]
         )
+    else:
+        from torchvision.transforms import Resize
+
+        base_transform.append(Resize(resize_to))
 
     dataset_transform = Compose(
         [
@@ -174,10 +293,8 @@ def create_dataset(
 
 
 def load_model(
-    label2id: dict[str, int], id2label: dict[int, str]
+    model_checkpoint: str, label2id: dict[str, int], id2label: dict[int, str]
 ) -> tuple[VideoMAEImageProcessor, VideoMAEForVideoClassification]:
-    model_checkpoint = "MCG-NJU/videomae-base"
-
     image_processor = VideoMAEImageProcessor.from_pretrained(model_checkpoint)
 
     model = VideoMAEForVideoClassification.from_pretrained(
@@ -185,7 +302,7 @@ def load_model(
         label2id=label2id,
         id2label=id2label,
         ignore_mismatched_sizes=True,  # provide this in case you're planning to fine-tune an already fine-tuned checkpoint
-    )
+    ).to("cuda")
 
     return image_processor, model
 
@@ -243,5 +360,39 @@ def display_gif(
     plt.show()
 
 
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--train",
+        "-T",
+        type=bool,
+        action=BooleanOptionalAction,
+        help="Whether a new model (based on huggingface's videomae base model) should be trained or not",
+    )
+    parser.add_argument(
+        "--name",
+        "-N",
+        type=str,
+        default="videomae-base-finetuned-ucf101-subset/checkpoint-740",
+        help="The name of the model. Will be used as the output directory when training a new model, or used to retrieve a pre-trained model from a directory with the same name in the project root",
+    )
+    parser.add_argument(
+        "--inference",
+        "-I",
+        type=bool,
+        action=BooleanOptionalAction,
+        help="Whether inference should be done on a test set with either the provided model or the newly trained model",
+    )
+    parser.add_argument(
+        "--demo",
+        "-D",
+        type=bool,
+        action=BooleanOptionalAction,
+        help="Whether a demo gif should be shown for each item being inferenced",
+    )
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
-    use_pretrained_hf_model()
+    args = parse_args()
+    main(args.train, args.name, args.inference, args.demo)
